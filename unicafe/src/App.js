05d887d02abbc33c677adbb137f35f5f70ef2ca2
ullallulla <aleksi.ojansivu@borealfoundry.com>
2021-03-19 17:00:37 +0200

    setupped app

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+
+
+const Display = props => (
+  <h1>give feedback</h1>
+)
+
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+
+)
+
+const Statistics = (props) => {
+
+
+  if (props.valueGood + props.valueNeutral + props.valueBad === 0) {
+    return (
+      <div>
+        <h1>
+          statistics
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          No feedback given
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      </div>
+    )
+
+  }
+
+  return (
+
+    <div>
+
+      <h1>statistics</h1>
+     
+      <table>
+        <tbody>
+          <Statistic text="good" value={props.valueGood} />
+          <Statistic text="neutral" value={props.valueNeutral} />
+          <Statistic text="bad" value={props.valueBad} />
+          <Statistic text="all" value={props.valueGood + props.valueNeutral + props.valueBad} />
+          <Statistic text="average" value={((props.valueGood * 1) + (props.valueNeutral * 0) + (props.valueBad * (-1))) / (props.valueGood + props.valueNeutral + props.valueBad)} />
+          <Statistic text="positive" value={(props.valueGood / (props.valueGood + props.valueNeutral + props.valueBad)) * 100} />
+        </tbody>
+      </table>
     </div>
-  );
+
+  )
+}
+
+
+const Statistic = (props) => {
+  if (props.text === 'positive') {
+    return (
+
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value} %</td>
+      </tr>
+
+    )
+  }
+  return (
+
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+
+  )
+}
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Display />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+
+      <Statistics valueGood={good} valueNeutral={neutral} valueBad={bad} all={good + neutral + bad} />
+
+
+    </div>
+  )
 }
 
 export default App;
